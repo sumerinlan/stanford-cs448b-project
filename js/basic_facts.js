3b2f04e0cf96819293bccd922f0f32fd0686deb6
Suyie Zhi <suyiezhi@gmail.com>
2020-03-16 01:34:37 -0700

    refactor

---
+++ js/basic_facts.js
@@ -0,0 +1,151 @@
+import {
+    CATEGORIES_1,
+    CATEGORIES_2
+} from './constants.js'
+
+var basicFactsTabActive = 'Hot Coffees';
+
+function setup() {
+    let basicFactsTabs = d3.select('#facts-tabs');
+    basicFactsTabs.selectAll('div').remove();
+
+    for (const i in CATEGORIES_1) {
+        let category1 = CATEGORIES_1[i];
+        // tab for one category
+        basicFactsTabs.append('div')
+            // don't display full name for Frappuccino
+            .text(category1.indexOf('Frappuccino') !== -1 ? 'Frappuccino' : category1)
+            .attr('class', category1 == basicFactsTabActive ? 'basic-facts-tabs-active' : 'basic-facts-tabs')
+            .on('click', function() {
+                basicFactsTabs.selectAll('div').attr('class', 'basic-facts-tabs');
+                basicFactsTabActive = category1;
+                d3.select(this).attr('class', 'basic-facts-tabs-active');
+                updateBasicFactsTabs();
+            });
+    }
+
+    // tab for all categories
+    basicFactsTabs.append('div')
+        .text('All Drinks')
+        .attr('class', basicFactsTabActive == null ? 'basic-facts-tabs-active' : 'basic-facts-tabs')
+        .on('click', function() {
+            basicFactsTabs.selectAll('div').attr('class', 'basic-facts-tabs');
+            basicFactsTabActive = null;
+            d3.select(this).attr('class', 'basic-facts-tabs-active');
+            updateBasicFactsTabs();
+        });
+}
+
+function updateBasicFactsTabs() {
+    // change 'display' to prevent reloading data too many times
+    d3.selectAll('.facts-category1')
+        .each(function() {
+            if (basicFactsTabActive == null) {
+                d3.select(this).style('display', 'block');
+            } else {
+                d3.select(this).style('display', d3.select(this).attr('id') == basicFactsTabActive ? 'block' : 'none');
+            }
+        })
+}
+
+function plot(data) {
+    let basicFacts = d3.select('#facts');
+    basicFacts.selectAll('div').remove();
+
+    let widget = d3.select('#facts-widget');
+
+    for (const i in CATEGORIES_1) {
+        let category1 = CATEGORIES_1[i];
+        let data1 = data.filter(d => d.CATEGORY1 == category1);
+        if (data1.length == 0) {
+            continue;
+        }
+
+        let container1 = basicFacts.append('div')
+            .attr('class', 'facts-category1')
+            .attr('id', category1);
+        // append header
+        container1.append('h2').text(category1);
+
+        for (const j in CATEGORIES_2[i][0]) {
+            let category2 = CATEGORIES_2[i][0][j];
+            const data2 = data.filter(d => d.CATEGORY2 == category2);
+            if (data2.length == 0) {
+                continue;
+            }
+
+            let container2 = container1.append('div');
+            // append header
+            container2.append('h3').text(category2);
+
+            // append images
+            let imgWidth = 96;
+            for (const da of data2) {
+                let images = container2.append('svg')
+                    .attr('class', 'facts-thumbnail-svg')
+                    .attr('width', imgWidth)
+                    .attr('height', imgWidth)
+                    .append('g')
+                    .on('mouseenter', d => {
+                        widget.style('display', 'block');
+                    })
+                    .on('mouseleave', d => {
+                        widget.style('display', 'none');
+                    })
+                    .selectAll('images')
+                    .data([da]);
+
+                images.enter()
+                    .append('image')
+                    .attr('width', imgWidth)
+                    .attr('height', imgWidth)
+                    .attr('x', 0)
+                    .attr('y', 0)
+                    .attr('xlink:href', d => d.IMAGE)
+                    .on('mouseenter', d => {
+                        d3.select('#facts-widget-img').attr('src', d.IMAGE);
+                        d3.select('#facts-widget-name').html(d.NAME);
+                        if (d.CALORIES == null) {
+                            d3.select('#facts-widget-calories').html('Not Available');
+                            d3.select('#facts-widget-cal-bar').attr('style', `width: 0%;`);
+                        } else {
+                            d3.select('#facts-widget-calories').html(d.CALORIES);
+                            d3.select('#facts-widget-cal-bar').attr('style', `width: ${d.CALORIES / 5}%;`);
+                        }
+                        if (d.SUGARS == null) {
+                            d3.select('#facts-widget-sugars').html('Not Available');
+                            d3.select('#facts-widget-sugar-bar').attr('style', `width: 0%;`);
+                        } else {
+                            d3.select('#facts-widget-sugars').html(d.SUGARS);
+                            d3.select('#facts-widget-sugar-bar').attr('style', `width: ${d.SUGARS / 65 * 100}%;`);
+                        }
+                    })
+                    .on('mousemove', d => {
+                        let margin = 15;
+                        let left = d3.event.pageX + margin;
+                        let top = d3.event.pageY + margin;
+                        // TODO: fix position
+                        // top = Math.min(window.innerHeight - 300 - margin, top);
+                        // top = Math.max(0, top);
+                        widget.style('left', `${left}px`).style('top', `${top}px`);
+                    });
+
+                images.exit().remove();
+
+
+
+                // let caption = container2.append('p')
+                //     .html("Cold Brew Coffee")
+                //     .attr('width', imgWidth)
+                //     .attr('height', imgWidth);
+            }
+        }
+    };
+
+    updateBasicFactsTabs();
+}
+
+export default {
+    setup,
+    plot
+}
---
+++ js/constants.js
@@ -0,0 +1,76 @@
+export const columns = {
+    TEMPERATURE: 'Temperature',
+    RANK: 'Rank',
+    CATEGORY1: 'Category1',
+    CATEGORY2: 'Category2',
+    NAME: 'Name',
+    SHORT_NAME: 'Short Name',
+    SIZE: 'Size',
+    IMAGE: 'Image',
+    CALORIES: 'Calories',
+    CALORIES_MODIFIED: 'Calories_Modified',
+    TOTAL_FAT: 'Total Fat (g)',
+    TRANS_FAT: 'Trans Fat (g)',
+    SATURATED_FAT: 'Saturated Fat (g)',
+    SODIUM: 'Sodium (mg)',
+    TOTAL_CARBOHYDRATES: 'Total Carbohydrates (g)',
+    CHOLESTEROL: 'Cholesterol (mg)',
+    DIETARY_FIBRE: 'Dietary Fibre (g)',
+    SUGARS: 'Sugars (g)',
+    CAFFEINE: 'Caffeine (mg)',
+}
+
+export const sizes = {
+    SHORT: 'Short',
+    TALL: 'Tall',
+    GRANDE: 'Grande',
+    VENTI: 'Venti',
+    SOLO: 'Solo',
+    DOPPIO: 'Doppio',
+    TRIPLE: 'Triple',
+    QUAD: 'Quad',
+}
+
+export const milks = {
+    NONFAT: 'Nonfat Milk',
+    TWO_PERCENT: '2% Milk',
+    WHOLE: 'Whole Milk',
+    SOYMILK: 'Soymilk',
+}
+
+export const CATEGORIES_COLORS = {
+    // spring
+    'Hot Coffees': '#c89543',
+    'Hot Teas': '#eed350',
+    'Hot Drinks': '#edc4c9',
+    'Frappuccino\xAE Blended Beverages': '#b6cbd5',
+    'Cold Coffees': '#c0d48b',
+    'Iced Teas': '#4c9f87',
+    'Cold Drinks': '#005e66'
+}
+
+export const CATEGORIES_1 = ['Hot Coffees', 'Hot Teas', 'Hot Drinks', 'Frappuccino\xAE Blended Beverages', 'Cold Coffees', 'Iced Teas', 'Cold Drinks'];
+
+export const CATEGORIES_2 = [
+    [
+        ['Americanos', 'Brewed Coffees', 'Cappuccinos', 'Espresso Shots', 'Flat Whites', 'Lattes', 'Macchiatos', 'Mochas', 'Clover® Brewed Coffees', 'Coffee Travelers']
+    ],
+    [
+        ['Chai Teas', 'Black Teas', 'Green Teas', 'Herbal Teas', 'White Teas']
+    ],
+    [
+        ['Hot Chocolates', 'Juice', 'Steamers', ]
+    ],
+    [
+        ['Coffee Frappuccino\xAE', 'Creme Frappuccino\xAE']
+    ],
+    [
+        ['Cold Brews', 'Iced Americano', 'Iced Coffees', 'Iced Espresso Shots', 'Iced Flat Whites', 'Iced Lattes', 'Iced Macchiatos', 'Iced Mochas', 'Iced Clover® Brewed Coffees']
+    ],
+    [
+        ['Iced Chai Teas', 'Iced Black Teas', 'Iced Green Teas', 'Iced Herbal Teas', 'Iced White Teas', 'Bottled Teas']
+    ],
+    [
+        ['Iced Coconutmilk Drinks', 'Starbucks Refreshers\u2122', 'Juice', 'Milk', 'Sparkling Water', 'Water']
+    ],
+];
